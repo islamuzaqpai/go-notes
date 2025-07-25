f1fa796853abb2f604c69e996c96b16247835029
islamuzaqpai <iuzakbayy@gmail.com>
2025-07-25 18:24:23 +0500

    Added frontend folder

---
+++ backend/cmd/server.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/islamuzaqpai/notes-app/internal/auth"
+	"github.com/islamuzaqpai/notes-app/internal/db"
+	"github.com/jackc/pgx/v5"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"strconv"
+)
+
+var conn *pgx.Conn
+
+func main() {
+	var err error
+	conn, err = db.Connect()
+	if err != nil {
+		log.Fatalf("Error connecting to db: %v", err)
+	}
+
+	router := gin.Default()
+
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
+	router.DELETE("/notes/:id", deleteNoteHandler)
+	router.PUT("/notes/:id", updateNoteHandler)
+
+	router.POST("/register", registrationHandler)
+	router.GET("/users", getUsersHandler)
+	router.POST("/login", loginHandler)
+
+	authorized := router.Group("/")
+	authorized.Use(auth.AuthMiddleware())
+
+	authorized.GET("/notes", getNotesHandler)
+	authorized.POST("/notes", createNoteHandler)
+
+	log.Println("Server is started on http://localhost:8080")
+	router.Run(":8080")
+}
+
+func getNotesHandler(c *gin.Context) {
+
+	userIdVal, exists := c.Get("userID")
+	if !exists {
+		c.JSON(500, gin.H{"error": "User ID not found"})
+		return
+	}
+
+	userId := userIdVal.(int)
+
+	notes, err := db.GetNotes(conn, userId)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Error when  getting notes"})
+		return
+	}
+	c.JSON(200, notes)
+}
+
+func createNoteHandler(c *gin.Context) {
+	userIdVal, exists := c.Get("userID")
+	if !exists {
+		c.JSON(500, gin.H{"error": "User ID not found"})
+		return
+	}
+
+	userId := userIdVal.(int)
+
+	var note db.Note
+	if err := c.BindJSON(&note); err != nil {
+		c.JSON(400, gin.H{"error": "Wrong JSON"})
+		return
+	}
+	note.UserId = userId
+
+	err := db.InsertNote(conn, note.Title, note.Content, note.UserId)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Error when creating a note"})
+		return
+	}
+	c.JSON(201, gin.H{"status": "ok"})
+}
+
+func updateNoteHandler(c *gin.Context) {
+	var note db.Note
+	idStr := c.Param("id")
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		panic(err)
+	}
+
+	err = c.BindJSON(&note)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid JSON"})
+		return
+	}
+
+	err = db.UpdateNote(conn, id, note.Title, note.Content)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Error Updating Note"})
+		return
+	}
+
+	c.JSON(200, gin.H{"status": "ok"})
+}
+
+func deleteNoteHandler(c *gin.Context) {
+	idStr := c.Param("id")
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.DeleteNote(conn, id)
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Error when deleting the note"})
+		return
+	}
+	c.JSON(200, gin.H{"status": "ok"})
+}
+
+func registrationHandler(c *gin.Context) {
+	var user db.User
+
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Wrong JSON"})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to hash password"})
+		return
+	}
+
+	err = db.InsertUser(conn, user.Username, user.Email, string(hashedPassword))
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Error inserting user"})
+		return
+	}
+	c.JSON(200, gin.H{"status": "ok"})
+}
+
+func getUsersHandler(c *gin.Context) {
+	users, err := db.GetUsers(conn)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Error when getting users"})
+		return
+	}
+	c.JSON(200, users)
+}
+
+func loginHandler(c *gin.Context) {
+	var user db.User
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Wrong JSON"})
+		return
+	}
+
+	dbUser, err := db.GetUserByEmail(conn, user.Email)
+	if err != nil {
+		c.JSON(401, gin.H{"error": "Invalid email error or password"})
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(user.Password))
+	if err != nil {
+		c.JSON(401, gin.H{"error": "Invalid email or password"})
+		return
+	}
+
+	token, err := auth.GenerateJWT(dbUser.ID)
+	if err != nil {
+		c.JSON(500, gin.H{"error": "Failed to generate toker"})
+		return
+	}
+
+	c.JSON(200, gin.H{"token": token})
+}
